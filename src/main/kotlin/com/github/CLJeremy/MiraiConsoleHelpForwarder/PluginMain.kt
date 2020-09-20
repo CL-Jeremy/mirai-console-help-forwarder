4db1eda61dd4cb146b119417c7db3c4f3b3536b4
Mike L <cl.jeremy@qq.com>
2020-09-21 00:25:27 +0200

    Switch to raw command; temp. fix permission target

--- src/main/kotlin/com/github/CLJeremy/MiraiConsoleHelpForwarder/PluginMain.kt
+++ src/main/kotlin/com/github/CLJeremy/MiraiConsoleHelpForwarder/PluginMain.kt
@@ -6,12 +6,13 @@ import com.google.auto.service.AutoService
 import net.mamoe.mirai.console.command.CommandManager.INSTANCE.register
 import net.mamoe.mirai.console.command.CommandManager.INSTANCE.unregister
 import net.mamoe.mirai.console.command.CommandSender
-import net.mamoe.mirai.console.command.SimpleCommand
+import net.mamoe.mirai.console.command.RawCommand
 import net.mamoe.mirai.console.permission.*
 import net.mamoe.mirai.console.permission.PermissionService.Companion.grantPermission
 import net.mamoe.mirai.console.plugin.jvm.JvmPlugin
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.message.data.MessageChain
 
 
 @AutoService(JvmPlugin::class)
@@ -24,7 +25,8 @@ object MyPluginMain : KotlinPlugin(
 
     override fun onEnable() {
         HelpCommand.register() // 注册指令
-        AbstractPermitteeId.AnyContact.grantPermission(HelpCommand.permission)
+        AbstractPermitteeId.AnyFriend.grantPermission(HelpCommand.permission)
+        AbstractPermitteeId.AnyGroup.grantPermission(HelpCommand.permission)
     }
 
     override fun onDisable() {
@@ -33,13 +35,12 @@ object MyPluginMain : KotlinPlugin(
 }
 
 // 简单指令
-object HelpCommand : SimpleCommand(
-    MyPluginMain, "help",
+object HelpCommand : RawCommand(
+    MyPluginMain, "helpcommand",
     description = "Help command to be forwarded"
 ) {
-    @Handler
-    suspend fun CommandSender.handle() { // 函数名随意, 但参数需要按顺序放置.
-        val verbatimCommand = "/help"
+    override suspend fun CommandSender.onCommand(args: MessageChain) {
+        val verbatimCommand = "/help ${args.joinToString(" ")}"
         MyPluginMain.logger.info("Forwarding $verbatimCommand")
         sendMessage(verbatimCommand)
     }
